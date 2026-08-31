09a86a5a4e9c093273ff36e27b41f0cabf603781
Arz03HUB <ArzziioX0X@outlook.com>
2026-08-31 13:49:56 -0400

    inr
    x

--- fabio_2/Program.cs
+++ fabio_2/Program.cs
@@ -1,10 +1,10 @@
 ﻿namespace fabio_2
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, fabio!");
         }
     }
 }
